768b3954168536a20c3dd90af927b3361790bc6d
yogesharumugam2103 <yogesharumugam2103@gmail.com>
2026-08-09 12:15:14 +0530

    Connect Flask to GitHub API

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/test-github")
+def test_github():
+    response = requests.get("https://api.github.com/users/octocat")
+    return response.json()
 
 if __name__ == "__main__":
     app.run(debug=True)
